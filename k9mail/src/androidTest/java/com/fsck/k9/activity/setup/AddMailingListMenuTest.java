dacc0ecafaf63d4d6f1e9e3df309e99ed9c49c52
Nirmal <nirmal_864@hotmail.com>
2019-02-10 11:32:53 -0500

    [#11] [#17] added cancel test in add mailing list

--- k9mail/src/androidTest/java/com/fsck/k9/activity/setup/AddMailingListMenuTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/activity/setup/AddMailingListMenuTest.java
@@ -27,7 +27,8 @@ public class AddMailingListMenuTest {
 
     @Rule
     public ActivityTestRule<MailingListMenu> mActivityTestRule = new ActivityTestRule<MailingListMenu>(MailingListMenu.class);
-    private String mailingListName = "test mailing list";
+    private String mailingListName = "mailing list test";
+    private String cancelled = "cancelled mailing list";
 
 
 
@@ -44,6 +45,7 @@ public class AddMailingListMenuTest {
 
         //find total number of items on mailing list menu and record it in a variable
         final int[] listCount = new int[1];
+
 
         Espresso.onView(withId(android.R.id.list)).check(matches(new TypeSafeMatcher<View>() {
             @Override
@@ -62,6 +64,7 @@ public class AddMailingListMenuTest {
             }
 
         }));
+        //Test add button
         //click on add button in mailing list
         Espresso.onView(withId(R.id.add_mailing_list)).perform(click());
 
@@ -76,10 +79,66 @@ public class AddMailingListMenuTest {
 
         //check to see that name appears on list
         Espresso.onData(anything()).inAdapterView(withId(android.R.id.list)).atPosition(listCount[0]).check(matches(withText(mailingListName)));
-
-
-
     }
+
+    @Test
+    public void cancelMailingList()
+    {
+        //find total number of items on mailing list menu and record it in a variable
+        final int[] listCount = new int[1];
+        final int[] updatedListCount = new int[1];
+
+        Espresso.onView(withId(android.R.id.list)).check(matches(new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(View view) {
+                ListView listView = (ListView) view;
+
+                //here we assume the adapter has been fully loaded already
+                listCount[0] = listView.getAdapter().getCount();
+
+                return true;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+
+            }
+
+        }));
+
+        //Test cancel button
+        //click on add button in mailing list
+        Espresso.onView(withId(R.id.add_mailing_list)).perform(click());
+
+        //Enter mailing list name in edit text
+        Espresso.onView(withId(R.id.mailingListName)).perform(typeText(cancelled));
+
+        //click on cancel button in dialog
+        Espresso.onView(withId(R.id.button_cancel)).perform(click());
+
+        //record total number of elements on list in new variable
+        Espresso.onView(withId(android.R.id.list)).check(matches(new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(View view) {
+                ListView listView = (ListView) view;
+
+                //here we assume the adapter has been fully loaded already
+                updatedListCount [0] = listView.getAdapter().getCount();
+
+                return true;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+
+            }
+
+        }));
+
+        //updated list count before and after cancelling should match
+        assertEquals(listCount[0], updatedListCount[0]);
+    }
+
     @After
     public void tearDown() throws Exception {
     }
