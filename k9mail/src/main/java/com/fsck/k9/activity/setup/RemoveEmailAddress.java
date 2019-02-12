28e68a0a9b933a1577878b78149df178b8c249c0
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-02-11 23:06:29 -0500

    #11, #21 - added RemoveEmailAddress

---
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/RemoveEmailAddress.java
@@ -0,0 +1,40 @@
+package com.fsck.k9.activity.setup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.fsck.k9.DaoSession;
+import com.fsck.k9.EmailAddress;
+import com.fsck.k9.K9;
+import com.fsck.k9.MailingList;
+import com.fsck.k9.R;
+
+public class RemoveEmailAddress extends Activity {
+
+    private DaoSession daoSession;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_email_address);
+
+        Intent intent = getIntent();
+        final Long emailAddressID = intent.getLongExtra("id", -1);
+
+        daoSession = ((K9) getApplication()).getDaoSession();
+        EmailAddress email = daoSession.getEmailAddressDao().loadByRowId(emailAddressID);
+        daoSession.delete(email);
+
+        MailingList toDelete = daoSession.getMailingListDao().loadByRowId(mailingListID);
+        //toDelete.setName(mailingListNameInput.getText().toString());
+        daoSession.getMailingListDao().delete(toDelete);
+        daoSession.clear();
+        Intent i = new Intent(getApplicationContext(), MailingListMenu.class);
+        i.putExtra("refresh needed", true);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        finish();
+        startActivity(i);
+        finish();
+    }
+
+}
