07680eee4918ef62e3750db64b2ab1a478c39fd3
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-03-02 04:43:15 -0500

    [#50] Checking if current date/time is recorded

--- k9mail/src/main/java/com/fsck/k9/activity/SetDateAndTime.java
+++ k9mail/src/main/java/com/fsck/k9/activity/SetDateAndTime.java
@@ -9,12 +9,23 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
 import com.fsck.k9.R;
+
+import java.util.Calendar;
 
 public class SetDateAndTime  extends K9Activity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
 
     private Button setTimeButton;
     private Button setDateButton;
+    private Button setDateAndTimeButton;
+
+    private int day = 0;
+    private int month = 0;
+    private int year = 0;
+    private int minute = 0;
+    private int hour = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -22,6 +33,7 @@ public class SetDateAndTime  extends K9Activity implements DatePickerDialog.OnDa
         setContentView(R.layout.send_later_set_date_and_time);
         setTimeButton = (Button) findViewById(R.id.send_later_set_date_button);
         setDateButton = (Button) findViewById(R.id.send_later_set_time_button);
+
 
         setDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +49,18 @@ public class SetDateAndTime  extends K9Activity implements DatePickerDialog.OnDa
                 newFragment.show(getFragmentManager(), "timePicker");
             }
         });
+        setDateAndTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+                int month = Calendar.getInstance().get(Calendar.MONTH);
+                int year = Calendar.getInstance().get(Calendar.YEAR);
+                int minute = Calendar.getInstance().get(Calendar.MINUTE);;
+                int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);;
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year + " | "  + hour + ":" + minute, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
@@ -44,6 +68,9 @@ public class SetDateAndTime  extends K9Activity implements DatePickerDialog.OnDa
         TextView date = (TextView) findViewById(R.id.send_later_date);
         String strDate = dayOfMonth + "/" + month + "/" + year;
         date.setText(strDate);
+        this.year = year;
+        this.month = month;
+        this.day = dayOfMonth
     }
 
     @Override
@@ -51,5 +78,7 @@ public class SetDateAndTime  extends K9Activity implements DatePickerDialog.OnDa
         TextView time = (TextView) findViewById(R.id.send_later_time);
         String strDate = hourOfDay + ":" + minute;
         time.setText(strDate);
+        this.hour = hourOfDay;
+        this.minute = minute;
     }
 }
