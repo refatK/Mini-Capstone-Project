d20884a6cb6534ec125847be46f2cff7244a2896
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-03-04 15:58:57 -0500

    #58 - created EmailsToSendNow

---
+++ k9mail/src/main/java/com/fsck/k9/EmailsToSendNow.java
@@ -0,0 +1,17 @@
+package com.fsck.k9;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class EmailsToSendNow extends IntentService {
+
+
+    public EmailsToSendNow() {
+        super("EmailsToSendNow");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent i) {
+
+    }
+}
