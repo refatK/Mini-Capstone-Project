0bcee37940c2aea16be75b54b2ab3b05732052b6
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-03-06 21:45:01 -0500

    [#50] Clean up

--- k9mail/src/androidTest/java/com/fsck/k9/activity/SetDateAndTimeTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/activity/SetDateAndTimeTest.java
@@ -1,6 +1,5 @@
 package com.fsck.k9.activity;
 
-import android.support.test.espresso.Espresso;
 import android.support.test.espresso.contrib.PickerActions;
 import android.support.test.rule.ActivityTestRule;
 import android.widget.DatePicker;
@@ -14,8 +13,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 
 import com.fsck.k9.R;
