5ef3132e417ca9d508b06f24e2285258eb72b035
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-03-28 20:49:55 -0400

    [#95] Created database entry for drunkmode

---
+++ k9mail/src/main/java/com/fsck/k9/DrunkMode.java
@@ -0,0 +1,56 @@
+package com.fsck.k9;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.Generated;
+
+import java.util.Date;
+
+@Entity(nameInDb = "drunk_mode")
+public class DrunkMode {
+
+    @Id(autoincrement = true)
+    private Long id;
+
+    @Property(nameInDb = "drunk")
+    private Boolean isDrunk;
+
+    @Property(nameInDb = "time")
+    private Date time;
+
+    @Generated(hash = 405359390)
+    public DrunkMode(Long id, Boolean isDrunk, Date time) {
+        this.id = id;
+        this.isDrunk = isDrunk;
+        this.time = time;
+    }
+
+    @Generated(hash = 1874578310)
+    public DrunkMode() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getIsDrunk() {
+        return this.isDrunk;
+    }
+
+    public void setIsDrunk(Boolean isDrunk) {
+        this.isDrunk = isDrunk;
+    }
+
+    public Date getTime() {
+        return this.time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}
