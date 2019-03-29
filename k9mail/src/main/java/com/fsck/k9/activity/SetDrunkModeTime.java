104d8c7a55ec418aa22cae33b71e25a887b272ec
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-03-28 20:24:53 -0400

    [#95] Created class for set drunk mode time

---
+++ k9mail/src/main/java/com/fsck/k9/activity/SetDrunkModeTime.java
@@ -0,0 +1,14 @@
+package com.fsck.k9.activity;
+
+import android.app.TimePickerDialog;
+import android.widget.TimePicker;
+
+import com.fsck.k9.R;
+
+public class SetDrunkModeTime extends K9Activity implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+}
