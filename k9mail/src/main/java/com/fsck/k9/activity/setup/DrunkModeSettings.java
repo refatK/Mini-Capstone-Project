4b61d82d22cbf4ac9d782d1543e14bbd30cd91b0
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-03-29 22:38:11 -0400

    [#95] Started adding Drunk Mode Menu

--- k9mail/src/main/java/com/fsck/k9/activity/setup/DrunkModeSettings.java
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/DrunkModeSettings.java
@@ -1,6 +1,15 @@
 package com.fsck.k9.activity.setup;
 
+import android.os.Bundle;
+
 import com.fsck.k9.activity.K9PreferenceActivity;
 
 public class DrunkModeSettings extends K9PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.drunk_mode_settings_preferences);
+    }
 }
