898b9a806dada9d57d21dafe59537d3383be15a6
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-03-29 23:04:00 -0400

    [#95] Added functionality to drunk mode settings

--- k9mail/src/main/java/com/fsck/k9/activity/setup/DrunkModeSettings.java
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/DrunkModeSettings.java
@@ -1,15 +1,54 @@
 package com.fsck.k9.activity.setup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 
+import com.fsck.k9.DaoSession;
+import com.fsck.k9.DrunkMode;
+import com.fsck.k9.K9;
 import com.fsck.k9.activity.K9PreferenceActivity;
+import com.fsck.k9.activity.SetDrunkModeTime;
+
+import com.fsck.k9.R;
 
 public class DrunkModeSettings extends K9PreferenceActivity {
+
+    private CheckBoxPreference isDrunkCheckbox;
+    private DaoSession daoSession;
+    private DrunkMode drunkModeSettings;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        daoSession = ((K9)getApplication()).getDaoSession();
+        drunkModeSettings = daoSession.getDrunkModeDao().loadByRowId(0);
+
         addPreferencesFromResource(R.xml.drunk_mode_settings_preferences);
+
+        isDrunkCheckbox = (CheckBoxPreference)findPreference("drunk_mode_settings_toggle");
+        isDrunkCheckbox.setChecked(drunkModeSettings.getIsDrunk());
+        isDrunkCheckbox.setOnPreferenceClickListener(
+                new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        Boolean isDrunk = !drunkModeSettings.getIsDrunk();
+                        isDrunkCheckbox.setChecked(isDrunk);
+                        drunkModeSettings.setIsDrunk(isDrunk);
+                        return true;
+                    }
+                }
+        );
+        findPreference("drunk_mode_settings_time").setOnPreferenceClickListener(
+                new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        startActivity(new Intent(getApplicationContext(), SetDrunkModeTime.class));
+                        return true;
+                    }
+                }
+        );
     }
 }
