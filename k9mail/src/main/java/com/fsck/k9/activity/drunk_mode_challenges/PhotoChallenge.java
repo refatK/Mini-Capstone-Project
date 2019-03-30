b439209a64e783b945081d37f95964cbebd5c245
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-03-29 22:26:19 -0400

    [#97] Added the activity for the photo challenge

---
+++ k9mail/src/main/java/com/fsck/k9/activity/drunk_mode_challenges/PhotoChallenge.java
@@ -0,0 +1,16 @@
+package com.fsck.k9.activity.drunk_mode_challenges;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.fsck.k9.R;
+
+public class PhotoChallenge extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_challenge);
+    }
+
+}
