bc6db2cde185bd82c717c1bd1d4c48d4545f9629
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-03-31 12:08:43 -0400

    #100 - created AudioChallenge

---
+++ k9mail/src/main/java/com/fsck/k9/activity/AudioChallenge.java
@@ -0,0 +1,15 @@
+package com.fsck.k9.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fsck.k9.R;
+
+public class AudioChallenge extends K9Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_challenge);
+    }
+}
