c2ebe0d60c19bd394888886880c9eb6b623803c0
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-03-30 21:25:37 -0400

    [#97] finalized the layout

--- k9mail/src/main/java/com/fsck/k9/activity/drunk_mode_challenges/PhotoChallenge.java
+++ k9mail/src/main/java/com/fsck/k9/activity/drunk_mode_challenges/PhotoChallenge.java
@@ -1,14 +1,29 @@
 package com.fsck.k9.activity.drunk_mode_challenges;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.app.Activity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fsck.k9.R;
+import com.fsck.k9.activity.K9Activity;
 
-public class PhotoChallenge extends Activity {
+public class PhotoChallenge extends K9Activity {
+    private Button choice1;
+    private Button choice2;
+    private Button choice3;
+    private Button choice4;
+    private ImageView mysteryPicture;
+    private String answer;
+    private String guess;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_challenge);
     }
