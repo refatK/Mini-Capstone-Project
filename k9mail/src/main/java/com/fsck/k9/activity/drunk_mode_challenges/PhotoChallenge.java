617b1656df80d47d275ce8e052f907a52bb5d809
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-03-30 23:37:46 -0400

    [#97] The image and the possible choices are loaded from the database

--- k9mail/src/main/java/com/fsck/k9/activity/drunk_mode_challenges/PhotoChallenge.java
+++ k9mail/src/main/java/com/fsck/k9/activity/drunk_mode_challenges/PhotoChallenge.java
@@ -1,17 +1,19 @@
 package com.fsck.k9.activity.drunk_mode_challenges;
 
-import android.graphics.Color;
-import android.graphics.Typeface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.fsck.k9.K9;
+import com.fsck.k9.Photo;
 import com.fsck.k9.R;
 import com.fsck.k9.activity.K9Activity;
+
 
 public class PhotoChallenge extends K9Activity {
     private Button choice1;
@@ -19,13 +21,28 @@ public class PhotoChallenge extends K9Activity {
     private Button choice3;
     private Button choice4;
     private ImageView mysteryPicture;
-    private String answer;
+    private Photo challengePhoto;
     private String guess;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_challenge);
+        challengePhoto = K9.daoSession.getPhotoDao().load(1L);
+        mysteryPicture = findViewById(R.id.imageView);
+        setChoices();
+        int challengePhotoID = getResources().getIdentifier(challengePhoto.getFileName(), null, this.getPackageName());
+        mysteryPicture.setImageResource(challengePhotoID);
     }
 
+    private void setChoices() {
+        choice1 = findViewById(R.id.choice1);
+        choice1.setText(challengePhoto.getChoice1());
+        choice2 = findViewById(R.id.choice2);
+        choice2.setText(challengePhoto.getChoice2());
+        choice3 = findViewById(R.id.choice3);
+        choice3.setText(challengePhoto.getChoice3());
+        choice4 = findViewById(R.id.choice4);
+        choice4.setText(challengePhoto.getChoice4());
+    }
 }
