a08c2e943a6e2a248959e4bada071ae1660c2a9d
Refat Kamal <refat.k17@gmail.com>
2019-04-02 04:13:28 -0400

    [#94, #98] make the signs use symbols [#94, #98] better code

--- k9mail/src/main/java/com/fsck/k9/activity/drunk_mode_challenges/MathChallenge.java
+++ k9mail/src/main/java/com/fsck/k9/activity/drunk_mode_challenges/MathChallenge.java
@@ -6,8 +6,13 @@ import android.widget.NumberPicker;
 import com.fsck.k9.activity.K9Activity;
 import com.fsck.k9.R;
 
-
 public class MathChallenge extends K9Activity {
+
+    public final String[] MATH_SIGNS = {"+", "-"};
+
+    private String sign = "+";
+    private int leftNumber;
+    private int rightNumber;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,19 +20,29 @@ public class MathChallenge extends K9Activity {
         setContentView(R.layout.activity_math_challenge);
 
         //Get number pickers
-        NumberPicker leftNumber = (NumberPicker) findViewById(R.id.left_number);
-        NumberPicker rightNumber = (NumberPicker) findViewById(R.id.right_number);
+        NumberPicker signInput = (NumberPicker) findViewById(R.id.sign);
+        NumberPicker leftNumberInput = (NumberPicker) findViewById(R.id.left_number);
+        NumberPicker rightNumberInput = (NumberPicker) findViewById(R.id.right_number);
 
-        //Set the minimum value of NumberPicker
-        leftNumber.setMinValue(0);
-        rightNumber.setMinValue(0);
-        //Specify the maximum value/number of NumberPicker
-        leftNumber.setMaxValue(9);
-        rightNumber.setMaxValue(9);
+        // set the number pickers up
+        setupMathInput(leftNumberInput, 0, 9);
+        setupMathInput(rightNumberInput, 0, 9);
+        setupMathInput(signInput, 0, MATH_SIGNS.length - 1);
 
-        //Gets whether the selector wheel wraps when reaching the min/max value.
-        leftNumber.setWrapSelectorWheel(true);
-        rightNumber.setWrapSelectorWheel(true);
+        // makes the input use array as display instead
+        signInput.setDisplayedValues(MATH_SIGNS);
+
+    }
+
+    private void setupMathInput(NumberPicker picker, int minVal, int maxVal) {
+        // sets min number or array start position
+        picker.setMinValue(minVal);
+
+        // sets max number or array end position
+        picker.setMaxValue(maxVal);
+
+        // makes the scroll wheel input wrap if min/max val reached
+        picker.setWrapSelectorWheel(true);
     }
 
 }
