3508a917630f1f850c1b30d2e2e67857c10f9b7a
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-04-01 22:28:56 -0400

    #100 - created getMediaPlayer to create a MediaPlayer that plays the correct audio file

--- k9mail/src/main/java/com/fsck/k9/activity/AudioChallenge.java
+++ k9mail/src/main/java/com/fsck/k9/activity/AudioChallenge.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 
 import com.fsck.k9.R;
 
+import org.jetbrains.annotations.Nullable;
+
 public class AudioChallenge extends K9Activity {
 
     private String answer = "";
@@ -44,6 +46,52 @@ public class AudioChallenge extends K9Activity {
     private void playSound()
     {
         final String answer = this.answer;
-        
+
+    }
+
+    @Nullable
+    private MediaPlayer getMediaPlayer(char ch)
+    {
+        switch (ch) {
+            case '0': return MediaPlayer.create(this, R.raw.n0);
+            case '1': return MediaPlayer.create(this, R.raw.n1);
+            case '2': return MediaPlayer.create(this, R.raw.n2);
+            case '3': return MediaPlayer.create(this, R.raw.n3);
+            case '4': return MediaPlayer.create(this, R.raw.n4);
+            case '5': return MediaPlayer.create(this, R.raw.n5);
+            case '6': return MediaPlayer.create(this, R.raw.n6);
+            case '7': return MediaPlayer.create(this, R.raw.n7);
+            case '8': return MediaPlayer.create(this, R.raw.n8);
+            case '9': return MediaPlayer.create(this, R.raw.n9);
+
+            case 'a': return MediaPlayer.create(this, R.raw.a);
+            case 'b': return MediaPlayer.create(this, R.raw.b);
+            case 'c': return MediaPlayer.create(this, R.raw.c);
+            case 'd': return MediaPlayer.create(this, R.raw.d);
+            case 'e': return MediaPlayer.create(this, R.raw.e);
+            case 'f': return MediaPlayer.create(this, R.raw.f);
+            case 'g': return MediaPlayer.create(this, R.raw.g);
+            case 'h': return MediaPlayer.create(this, R.raw.h);
+            case 'i': return MediaPlayer.create(this, R.raw.i);
+            case 'j': return MediaPlayer.create(this, R.raw.j);
+            case 'k': return MediaPlayer.create(this, R.raw.k);
+            case 'l': return MediaPlayer.create(this, R.raw.l);
+            case 'm': return MediaPlayer.create(this, R.raw.m);
+            case 'n': return MediaPlayer.create(this, R.raw.n);
+            case 'o': return MediaPlayer.create(this, R.raw.o);
+            case 'p': return MediaPlayer.create(this, R.raw.p);
+            case 'q': return MediaPlayer.create(this, R.raw.q);
+            case 'r': return MediaPlayer.create(this, R.raw.r);
+            case 's': return MediaPlayer.create(this, R.raw.s);
+            case 't': return MediaPlayer.create(this, R.raw.t);
+            case 'u': return MediaPlayer.create(this, R.raw.u);
+            case 'v': return MediaPlayer.create(this, R.raw.v);
+            case 'w': return MediaPlayer.create(this, R.raw.w);
+            case 'x': return MediaPlayer.create(this, R.raw.x);
+            case 'y': return MediaPlayer.create(this, R.raw.y);
+            case 'z': return MediaPlayer.create(this, R.raw.z);
+        }
+
+        return null;
     }
 }
