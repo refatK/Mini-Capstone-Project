1b7d53a0fba9a951270331c7103c23d96741b24e
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-04-04 01:32:37 -0400

    [#94, 97] added the test for when you click the first wrong answer

--- k9mail/src/androidTest/java/_390Tests/Release3/PhotoChallengeTestUI.java
+++ k9mail/src/androidTest/java/_390Tests/Release3/PhotoChallengeTestUI.java
@@ -1,8 +1,15 @@
 package _390Tests.Release3;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,10 +18,28 @@ import android.widget.TextView;
 import com.fsck.k9.R;
 import com.fsck.k9.activity.drunk_mode_challenges.PhotoChallenge;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.assertion.ViewAssertions.selectedDescendantsMatch;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.hasBackground;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.hasTextColor;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
 
 public class PhotoChallengeTestUI {
     /* Tests that the challenge behaves correctly based on what's being displayed*/
@@ -34,7 +59,6 @@ public class PhotoChallengeTestUI {
         Intent pc = new Intent();
         Bundle testPack = new Bundle();
         testPack.putBoolean("test", true);
-        testPack.putBoolean("Practice", true);
         pc.putExtras(testPack);
         photoC.launchActivity(pc);
         setFields(photoC.getActivity());
@@ -46,11 +70,26 @@ public class PhotoChallengeTestUI {
         choice2 = activity.findViewById(R.id.choice2);
         choice3 = activity.findViewById(R.id.choice3);
         choice4 = activity.findViewById(R.id.choice4);
-        challengePhoto = activity.findViewById(R.id.prompt);
+        challengePhoto = activity.findViewById(R.id.imageView);
     }
 
     @Test
-    public void testChooseWrongAnswer() {
-
+    public void testChooseFirstWrongAnswer() {
+        Espresso.onView(withId(challengePhoto.getId())).check(matches(isDisplayed()));
+        Espresso.onView(withId(choice1.getId())).check(matches(isClickable()));
+        Espresso.onView(withId(choice2.getId())).check(matches(isClickable()));
+        Espresso.onView(withId(choice3.getId())).check(matches(isClickable()));
+        Espresso.onView(withId(choice4.getId())).check(matches(isClickable()));
+        Espresso.onView(withText("Wolf")).perform(click());
+        Espresso.onView(withId(choice1.getId())).check(matches(withText("Wolf ❌")));
+        Espresso.onView(withId(choice2.getId())).check(matches(withText("Fox")));
+        Espresso.onView(withId(choice3.getId())).check(matches(withText("Dog")));
+        Espresso.onView(withId(choice4.getId())).check(matches(withText("Coyote")));
+        Espresso.onView(withId(prompt.getId())).check(matches(withText(R.string.photo_challenge_failed)));
+        Espresso.onView(withId(prompt.getId())).check(matches(hasTextColor(android.R.color.white)));
+        Espresso.onView(withId(choice1.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.white), withId(choice1.getId())));
+        Espresso.onView(withId(choice2.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice2.getId())));
+        Espresso.onView(withId(choice3.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice3.getId())));
+        Espresso.onView(withId(choice4.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice4.getId())));
     }
 }
