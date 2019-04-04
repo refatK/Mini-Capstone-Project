231a91c9701d60d2cf1629116a33bd492fec7ccf
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-04-04 01:57:35 -0400

    [#94, 97] added the test for the timeout after 10 seconds

--- k9mail/src/androidTest/java/_390Tests/Release3/PhotoChallengeTestUI.java
+++ k9mail/src/androidTest/java/_390Tests/Release3/PhotoChallengeTestUI.java
@@ -1,15 +1,11 @@
 package _390Tests.Release3;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.ViewAssertion;
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.matcher.ViewMatchers;
+
 import android.support.test.rule.ActivityTestRule;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,28 +14,20 @@ import android.widget.TextView;
 import com.fsck.k9.R;
 import com.fsck.k9.activity.drunk_mode_challenges.PhotoChallenge;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.assertion.ViewAssertions.selectedDescendantsMatch;
-import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
-import static android.support.test.espresso.matcher.ViewMatchers.hasBackground;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static android.support.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.not;
+import static java.lang.Thread.sleep;
 
 public class PhotoChallengeTestUI {
     /* Tests that the challenge behaves correctly based on what's being displayed*/
@@ -154,5 +142,25 @@ public class PhotoChallengeTestUI {
         Espresso.onView(withId(choice3.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice3.getId())));
         Espresso.onView(withId(choice4.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice4.getId())));
 
+    }
+    @Test
+    public void testTimeout() {
+        Looper.prepare();
+        Espresso.onView(withId(challengePhoto.getId())).check(matches(isDisplayed()));
+        Espresso.onView(withId(choice1.getId())).check(matches(isClickable()));
+        Espresso.onView(withId(choice2.getId())).check(matches(isClickable()));
+        Espresso.onView(withId(choice3.getId())).check(matches(isClickable()));
+        Espresso.onView(withId(choice4.getId())).check(matches(isClickable()));
+        try {
+            sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Espresso.onView(withId(prompt.getId())).check(matches(withText(R.string.photo_challenge_timeout)));
+        Espresso.onView(withId(prompt.getId())).check(matches(hasTextColor(android.R.color.black)));
+        Espresso.onView(withId(choice1.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice1.getId())));
+        Espresso.onView(withId(choice2.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice2.getId())));
+        Espresso.onView(withId(choice3.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice3.getId())));
+        Espresso.onView(withId(choice4.getId())).check(selectedDescendantsMatch(hasTextColor(android.R.color.black), withId(choice4.getId())));
     }
 }
