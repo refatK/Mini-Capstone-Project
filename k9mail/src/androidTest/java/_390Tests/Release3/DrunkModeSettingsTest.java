4cdf219c6eb86d2e8fa22e4f10b6b0ffae444d5d
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-04-06 16:10:44 -0400

    [#129] Testing the toggles

--- k9mail/src/androidTest/java/_390Tests/Release3/DrunkModeSettingsTest.java
+++ k9mail/src/androidTest/java/_390Tests/Release3/DrunkModeSettingsTest.java
@@ -6,9 +6,39 @@ import com.fsck.k9.R;
 import com.fsck.k9.activity.setup.DrunkModeSettings;
 
 import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 public class DrunkModeSettingsTest {
 
     @Rule
     public ActivityTestRule<DrunkModeSettings> mActivityTestRule = new ActivityTestRule<>(DrunkModeSettings.class);
+
+    @Test
+    public void testSetDrunkTime(){
+        onData(withKey("drunk_mode_settings_time")).check(matches(isDisplayed()));
+    }
+
+    //Check if toggle displayed
+    @Test
+    public void testToggleDrunk(){
+        onData(withKey("drunk_mode_settings_toggle")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testToggleDrunkOnce(){
+        onData(withKey("drunk_mode_settings_toggle")).perform(click());
+    }
+
+    //Check if check mark if toggle works
+    @Test
+    public void testToggleDrunkTwice(){
+        onData(withKey("drunk_mode_settings_toggle")).perform(click());
+        onData(withKey("drunk_mode_settings_toggle")).perform(click());
+    }
 }
