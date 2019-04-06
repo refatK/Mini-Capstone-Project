ffe83bea2063152afe944186c705c41ec3cbc9df
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-04-06 13:40:07 -0400

    [#129] Added template for UI test

--- k9mail/src/test/java/com/fsck/k9/_390Tests/Release3/DrunkModeSettingsTest.java
+++ k9mail/src/test/java/com/fsck/k9/_390Tests/Release3/DrunkModeSettingsTest.java
@@ -1,9 +1,27 @@
 package com.fsck.k9._390Tests.Release3;
 
 import com.fsck.k9.K9RobolectricTestRunner;
+import com.fsck.k9.activity.setup.DrunkModeSettings;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.mockito.Mockito.mock;
 
 @RunWith(K9RobolectricTestRunner.class)
 public class DrunkModeSettingsTest {
+
+    //to be tested
+    private DrunkModeSettings drunkModeSettings;
+
+    @Before
+    public void setUp(){
+        drunkModeSettings = mock(DrunkModeSettings.class);
+    }
+
+    @Test
+    public void testDateToCalendarFormat(){
+
+    }
 }
--- k9mail/src/test/java/com/fsck/k9/_390Tests/Release3/SetDrunkModeTimeTest.java
+++ k9mail/src/test/java/com/fsck/k9/_390Tests/Release3/SetDrunkModeTimeTest.java
@@ -1,9 +1,42 @@
 package com.fsck.k9._390Tests.Release3;
 
 import com.fsck.k9.K9RobolectricTestRunner;
+import com.fsck.k9.activity.SetDrunkModeTime;
+import com.fsck.k9.activity.setup.DrunkModeSettings;
+import com.fsck.k9.fragment.DrunkModeTimePicker;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.mockito.Mockito.mock;
 
 @RunWith(K9RobolectricTestRunner.class)
 public class SetDrunkModeTimeTest {
+
+    //To be tested
+    private DrunkModeTimePicker drunkModeTimePicker;
+    private SetDrunkModeTime setDrunkModeTime;
+
+    @Before
+    public void setUp(){
+        drunkModeTimePicker = mock(DrunkModeTimePicker.class);
+        setDrunkModeTime = mock(SetDrunkModeTime.class);
+    }
+
+    @Test
+    public void testDateToCalendarFormat(){
+
+    }
+
+    @Test
+    public void testConfirmSetTime(){
+
+    }
+
+    @Test
+    public void testOnTimeSet(){
+
+    }
+
 }
