17953fa861165ae8e7a1861b0381e2fe6a59af88
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-04-06 12:24:20 -0400

    [#129] Setup of Tests

---
+++ k9mail/src/androidTest/java/_390Tests/Release3/SetDrunkModeTimeTest.java
@@ -0,0 +1,6 @@
+package _390Tests.Release3;
+
+import com.fsck.k9.R;
+
+public class SetDrunkModeTimeTest {
+}
---
+++ k9mail/src/test/java/com/fsck/k9/_390Tests/Release3/DrunkModeOptionsTest.java
@@ -0,0 +1,9 @@
+package com.fsck.k9._390Tests.Release3;
+
+import com.fsck.k9.K9RobolectricTestRunner;
+
+import org.junit.runner.RunWith;
+
+@RunWith(K9RobolectricTestRunner.class)
+public class DrunkModeOptionsTest {
+}
