81ff73cd15218a8131ca0999a6eb40705ce345e3
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-04-09 14:08:22 -0400

    #103, #104 - created SetFollowUpReminderDateAndTime

---
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/SetFollowUpReminderDateAndTime.java
@@ -0,0 +1,18 @@
+package com.fsck.k9.activity.setup;
+
+import android.os.Bundle;
+import com.fsck.k9.activity.K9Activity;
+
+import com.fsck.k9.R;
+
+
+
+public class SetFollowUpReminderDateAndTime extends K9Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_follow_up_reminder_date_and_time);
+    }
+
+}
