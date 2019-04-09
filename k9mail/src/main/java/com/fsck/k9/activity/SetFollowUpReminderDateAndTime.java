03cbdee387af23a76c09b6f6f8e2a464eae7bfde
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-04-09 16:06:59 -0400

    #103, #104 - created layout files

--- k9mail/src/main/java/com/fsck/k9/activity/SetFollowUpReminderDateAndTime.java
+++ k9mail/src/main/java/com/fsck/k9/activity/SetFollowUpReminderDateAndTime.java
@@ -1,13 +1,19 @@
 package com.fsck.k9.activity;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.fsck.k9.DaoSession;
 import com.fsck.k9.R;
+import com.fsck.k9.fragment.SendLaterDatePicker;
+import com.fsck.k9.fragment.SendLaterTimePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 
 public class SetFollowUpReminderDateAndTime extends K9Activity {
@@ -19,12 +25,18 @@ public class SetFollowUpReminderDateAndTime extends K9Activity {
     private TextView chosenDateTextView;
     private TextView chosenTimeTextView;
 
+    private Calendar chosenDateAndTime;
+
     private DaoSession daoSession;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_follow_up_reminder_date_and_time);
+        setTimeButton = (Button) findViewById(R.id.reminder_set_time_button);
+        setDateButton = (Button) findViewById(R.id.reminder_set_date_button);
+        setDateAndTimeButton = (Button) findViewById(R.id.reminder_set_date_and_time_button);
+
     }
 
 }
