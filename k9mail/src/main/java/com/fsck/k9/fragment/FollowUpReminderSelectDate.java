021008798ddb771e0c1ceef7ac7ed07a2430eb5b
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-04-09 16:52:27 -0400

    #103, #104 - created FollowUpReminderSelectDate

---
+++ k9mail/src/main/java/com/fsck/k9/fragment/FollowUpReminderSelectDate.java
@@ -0,0 +1,27 @@
+package com.fsck.k9.fragment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+import com.fsck.k9.activity.SetFollowUpReminderDateAndTime;
+
+import java.util.Calendar;
+
+
+public class FollowUpReminderSelectDate extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dpd = new DatePickerDialog(getActivity(), (SetFollowUpReminderDateAndTime)getActivity(), year, month, day);
+        dpd.setTitle("Set Date for Follow-up Reminder");
+        return dpd;
+    }
+
+}
