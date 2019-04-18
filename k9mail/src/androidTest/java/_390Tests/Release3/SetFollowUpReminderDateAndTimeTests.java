bce46e5a5da18a2604ef5baa537e0553f0b79d90
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-04-17 23:27:06 -0400

    #103, #104 - added attributes for testing

--- k9mail/src/androidTest/java/_390Tests/Release3/SetFollowUpReminderDateAndTimeTests.java
+++ k9mail/src/androidTest/java/_390Tests/Release3/SetFollowUpReminderDateAndTimeTests.java
@@ -1,5 +1,6 @@
 package _390Tests.Release3;
 
+import android.content.Intent;
 import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 
@@ -9,6 +10,8 @@ import com.fsck.k9.activity.drunk_mode_challenges.AudioChallenge;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.Calendar;
 
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
@@ -22,4 +25,13 @@ public class SetFollowUpReminderDateAndTimeTests {
     @Rule
     public ActivityTestRule<SetFollowUpReminderDateAndTime> testRule = new ActivityTestRule<SetFollowUpReminderDateAndTime>(SetFollowUpReminderDateAndTime.class);
 
+    private String strDate;
+    private String strTime;
+    private Calendar calendar;
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+    private Intent intent;
 }
