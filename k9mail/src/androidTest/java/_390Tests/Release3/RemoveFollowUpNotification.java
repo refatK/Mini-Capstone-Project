ad35b6cb19fe13a275ec8d58d8913308ae48442b
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-04-21 14:49:45 -0400

    [#106] Added test for removing Followup notifications

---
+++ k9mail/src/androidTest/java/_390Tests/Release3/RemoveFollowUpNotification.java
@@ -0,0 +1,29 @@
+package _390Tests.Release3;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+
+import com.fsck.k9.activity.FollowUpNotificationsList;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
+public class RemoveFollowUpNotification {
+    @Rule
+    public ActivityTestRule<FollowUpNotificationsList> testRule = new ActivityTestRule<>(FollowUpNotificationsList.class);
+
+    @Test
+    public void removeQuickReplyTest()
+    {
+        Espresso.onView(withId(android.R.id.list));
+        onData(anything()).inAdapterView(withId(android.R.id.list)).atPosition(0).perform(longClick());
+        Espresso.onView(withText("Delete")).perform(click());
+    }
+}
