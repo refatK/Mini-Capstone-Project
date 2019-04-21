07a49224c951fec7a60ff8859b0f2b8b282faf1b
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-04-21 10:57:51 -0400

    [#106] Created activity to delete FN

---
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/RemoveFollowUpNotification.java
@@ -0,0 +1,14 @@
+package com.fsck.k9.activity.setup;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.fsck.k9.R;
+
+public class RemoveFollowUpNotification extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_follow_up_notification);
+    }
+}
