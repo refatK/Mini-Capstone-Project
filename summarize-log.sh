1c38b40b6fe4d9558358902cba37acc6c326ec42
Refat Kamal <refat.k17@gmail.com>
2019-04-23 05:57:12 -0400

    [#103, #111] try to add more info to summary

--- summarize-log.sh
+++ summarize-log.sh
@@ -4,8 +4,18 @@ URL="https://0x0.st"
 FILE=travisOutput.txt
 SUMMARY=summary.txt
 
-echo "Summarizing Travis Log for Test/Build Failures"
-awk '/> Task :k9mail:compileDebugJavaWithJavac FAILED|> Task :k9mail:testDebugUnitTest/,/FAILURE: Build failed with an exception./' ${FILE} > ${SUMMARY}
+# remove the summary if it already exists
+rm -f summary.txt
 
+# set up info for summary
+echo "Summarizing Travis Log for Test/Build Failures" >> ${SUMMARY}
+echo "----------------------------------------------" >> ${SUMMARY}
+echo "" >> ${SUMMARY}
+echo "Branch: ${TRAVIS_BRANCH}" >> ${SUMMARY}
+echo "Commit: ${TRAVIS_COMMIT} - ${TRAVIS_COMMIT_MESSAGE}" >> ${SUMMARY}
+echo "" >> ${SUMMARY}
+awk '/> Task :k9mail:compileDebugJavaWithJavac FAILED|> Task :k9mail:testDebugUnitTest/,/FAILURE: Build failed with an exception./' ${FILE} >> ${SUMMARY}
+
+# print to an outside source to view summary
 RESPONSE=$(curl -# -F "file=@${SUMMARY}" "${URL}")
 echo "${RESPONSE}"
