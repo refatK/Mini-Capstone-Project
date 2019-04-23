bf95b6257598200358696fce854e5dd09ac14b87
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-04-23 00:22:24 -0400

    [#103, #108] fixing merge conflicts - part 2

--- k9mail/src/main/java/com/fsck/k9/activity/FollowUpNotificationsList.java
+++ k9mail/src/main/java/com/fsck/k9/activity/FollowUpNotificationsList.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 
