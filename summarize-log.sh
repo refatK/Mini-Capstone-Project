2e8ca11c83fa0c86c0c78a48f8f89f974e32f887
Refat Kamal <refat.k17@gmail.com>
2019-04-23 06:16:49 -0400

    [#103, #111] move patterns to variables

--- summarize-log.sh
+++ summarize-log.sh
@@ -4,6 +4,9 @@ URL="https://0x0.st"
 FILE=travisOutput.txt
 SUMMARY=summary.txt
 
+START_PAT="/> Task :k9mail:compileDebugJavaWithJavac FAILED|> Task :k9mail:testDebugUnitTest/"
+END_PAT="/FAILURE: Build failed with an exception./"
+
 # remove the summary if it already exists
 rm -f summary.txt
 
@@ -15,9 +18,12 @@ echo "Branch: ${TRAVIS_BRANCH}" >> ${SUMMARY}
 echo "Commit: ${TRAVIS_COMMIT} - ${TRAVIS_COMMIT_MESSAGE}" >> ${SUMMARY}
 echo "Caused By: ${AUTHOR_NAME}" >> ${SUMMARY}
 echo "" >> ${SUMMARY}
+echo "" >> ${SUMMARY}
 echo "--- FAILURE ---" >> ${SUMMARY}
 echo "**************************" >> ${SUMMARY}
-awk '/> Task :k9mail:compileDebugJavaWithJavac FAILED|> Task :k9mail:testDebugUnitTest/,/FAILURE: Build failed with an exception./' ${FILE} >> ${SUMMARY}
+echo "" >> ${SUMMARY}
+awk '${START_PAT}{flag=1} ${END_PAT}{flag=0} flag' ${FILE} >> ${SUMMARY}
+echo "" >> ${SUMMARY}
 echo "**************************" >> ${SUMMARY}
 
 # print to an outside source to view summary
